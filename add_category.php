190a0b699702a8b89215b3c4fed4ee47bfea3af0
APTASU <122052558+APTASU@users.noreply.github.com>
2023-05-18 17:25:57 -0500

    Update add_category.php

--- add_category.php
+++ add_category.php
@@ -1,23 +1,42 @@
 <?php
-// Get the category data
-$name = filter_input(INPUT_POST, 'name');
+// Get the player data
+$playerID = filter_input(INPUT_POST, 'player_id');
+$playerFname = filter_input(INPUT_POST, 'player_fname');
+$playerLname = filter_input(INPUT_POST, 'player_lname');
+$street1 = filter_input(INPUT_POST, 'street1');
+$street2 = filter_input(INPUT_POST, 'street2');
+$zipCode = filter_input(INPUT_POST, 'zip_code');
+$state = filter_input(INPUT_POST, 'state');
 
 // Validate inputs
-if ($name == null) {
-    $error = "Invalid category data. Check all fields and try again.";
+if (
+    $playerID == null ||
+    $playerFname == null ||
+    $playerLname == null ||
+    $street1 == null ||
+    $zipCode == null ||
+    $state == null
+) {
+    $error = "Invalid player data. Check all fields and try again.";
     include('error.php');
 } else {
     require_once('database.php');
 
-    // Add the product to the database  
-    $query = 'INSERT INTO categories (categoryName)
-              VALUES (:category_name)';
+    // Add the player to the database
+    $query = 'INSERT INTO Beta_Tester (PlayerID, PlayerFname, PlayerLname, Street1, Street2, ZipCode, State)
+              VALUES (:player_id, :player_fname, :player_lname, :street1, :street2, :zip_code, :state)';
     $statement = $db->prepare($query);
-    $statement->bindValue(':category_name', $name);
+    $statement->bindValue(':player_id', $playerID);
+    $statement->bindValue(':player_fname', $playerFname);
+    $statement->bindValue(':player_lname', $playerLname);
+    $statement->bindValue(':street1', $street1);
+    $statement->bindValue(':street2', $street2);
+    $statement->bindValue(':zip_code', $zipCode);
+    $statement->bindValue(':state', $state);
     $statement->execute();
     $statement->closeCursor();
 
-    // Display the Category List page
-    include('category_list.php');
+    // Display the Player List page
+    include('player_list.php');
 }
 ?>
