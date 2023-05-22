c6e6ea8b5d9e47866cc05b83c9df8892a58bc743
APTASU <122052558+APTASU@users.noreply.github.com>
2023-05-21 19:28:15 -0500

    Update delete_product.php

--- delete_product.php
+++ delete_product.php
@@ -1,17 +1,20 @@
+<?php
 require_once('database.php');
 
-// Get IDs
+// Get the player ID
 $player_id = filter_input(INPUT_POST, 'player_id', FILTER_VALIDATE_INT);
 
 // Delete the player from the database
-if ($player_id != false) {
+if ($player_id !== false) {
     $query = 'DELETE FROM Beta_Tester
               WHERE PlayerID = :player_id';
     $statement = $db->prepare($query);
     $statement->bindValue(':player_id', $player_id);
     $success = $statement->execute();
-    $statement->closeCursor();    
+    $statement->closeCursor();
 }
 
-// Display the Player List page
-include('index.php');
+// Redirect back to the Player List page
+header("Location: index.php");
+exit();
+?>
