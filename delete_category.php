7f200540096718dbdfe0ac8a22415599be7ca0a8
APTASU <122052558+APTASU@users.noreply.github.com>
2023-05-21 19:28:59 -0500

    Update delete_category.php

--- delete_category.php
+++ delete_category.php
@@ -3,7 +3,7 @@
 $gameID = filter_input(INPUT_POST, 'game_id', FILTER_VALIDATE_INT);
 
 // Validate inputs
-if ($gameID == null || $gameID == false) {
+if ($gameID === null || $gameID === false) {
     $error = "Invalid game ID.";
     include('error.php');
 } else {
@@ -13,10 +13,11 @@ if ($gameID == null || $gameID == false) {
     $query = 'DELETE FROM Game WHERE GameID = :game_id';
     $statement = $db->prepare($query);
     $statement->bindValue(':game_id', $gameID);
-    $statement->execute();
+    $success = $statement->execute();
     $statement->closeCursor();
 
-    // Display the Game List page
-    include('game_list.php');
+    // Redirect to the Game List page
+    header("Location: game_list.php");
+    exit();
 }
 ?>
